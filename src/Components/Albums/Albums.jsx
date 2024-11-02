f4768b69ca62aaab7969d8c2813b899fc8c34608
Nishit Rana <nishitrana@Nishits-MacBook-Air.local>
2024-11-02 19:44:19 +0530

    Carousel update

--- src/Components/Albums/Albums.jsx
+++ src/Components/Albums/Albums.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Section from '../Section/Section';
 const TopAlbums = () => <Section title="Top Albums" apiEndpoint="https://qtify-backend-labs.crio.do/albums/top" />;
 const NewAlbums = () => <Section title="New Albums" apiEndpoint="https://qtify-backend-labs.crio.do/albums/new" />;
+const Songs = () => <Section title="Songs" apiEndpoint="https://qtify-backend-labs.crio.do/songs"/>;
 
 const AlbumsPage = () => {
   return (
     <div>
       <TopAlbums />
       <NewAlbums />
+      <Songs />
     </div>
   );
 };
